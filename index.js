b7fb005e839f44c92982a0dc5a63d2015abe0c06
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-07-09 15:17:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ app.post('/delete/:filename', (req, res) => {
     });
 });
 
-app.listen(3000, function () {
-    console.log('Server is running on port 3000');
+app.listen(8000, function () {
+    console.log('Server is running on port 8000');
 });
